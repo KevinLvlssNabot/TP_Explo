5b29a209fce8e368e930a015ea3e0bc607097350
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-02-03 09:37:51 +0100

    Choix par nom

--- explo.c
+++ explo.c
@@ -6,14 +6,18 @@
 
 
 int main(int argc, char const *argv[]) {
-  
-  int choixVille;
+
+  char choixVille[15];
+  char nomVille[15][15] = {"Belese","Alqueir"};
+
+
+
   printf("Dans quelle ville souhaitez-vous vous rendre ? Belese [1], Alqueir[2]\n");
-  scanf("%d", &choixVille);
-      if (choixVille == 1) {
-        printf("Vous avez atteint la ville de Belese.\n");
-      } else if (choixVille == 2) {
-        printf("Vous avez atteint la ville d'Alqueir.\n");
+  scanf("%s", choixVille);
+      if (strcmp(nomVille[0], choixVille) == 0) {
+        printf("Vous avez atteint la ville de %s.\n", nomVille[0]);
+      } else if (strcmp(nomVille[1], choixVille) == 0) {
+        printf("Vous avez atteint la ville d'%s.\n", nomVille[1]);
       }
 
   return 0;
