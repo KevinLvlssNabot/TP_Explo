5e7ffd3aacc000552d3580d331e727c37ea1e59c
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-02-03 12:58:12 +0100

    Gestion de l'endurance.

--- explo.c
+++ explo.c
@@ -40,7 +40,11 @@ struct Villes_t{
 //      }
 //}
 
-
+void Color(int t,int f)
+{
+    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H,f*16+t);
+}
 
 
 int main(int argc, char const *argv[]) {
@@ -67,37 +71,91 @@ int main(int argc, char const *argv[]) {
 
   int choixID;
   int continuer = 1;
+  int endurance = 100;
+  int avantPoste = 0;
+  char pancarte[100] = "Il y a un avant poste ici qui reduit la difficulte des lieux.";
+  int repos = 50;
+  int choixRepos = 0;
 //  char lieuActuel[15];
 
 //  deplacementVoyage(carte.villes_list->nom, choixVille[15], carte.villes_list->description);
  // demande initiale
-  printf("Dans quelle ville souhaitez-vous vous rendre ? %s [1], %s[2], %s[3], %s[4,], %s[5]\n", belese.nom, alqueir.nom, zoinrel.nom, ya.nom, sirasp.nom);
+  printf("Dans quelle ville souhaitez-vous vous rendre ? Entrez le nom de la ville. %s [1], %s[2], %s[3], %s[4,], %s[5]\n", belese.nom, alqueir.nom, zoinrel.nom, ya.nom, sirasp.nom);
   scanf("%s", choixVille);
       if (strcmp(belese.nom, choixVille) == 0) {
         printf("Vous avez atteint la ville de %s.\n", belese.nom);
         printf("%s\n", belese.description);
         strcpy(lieuActuel.nom, belese.nom);
         lieuActuel.ID = belese.ID;
+        lieuActuel.difficulte = belese.difficulte;
+        Color(5,0);
+        endurance = endurance - lieuActuel.difficulte;
+        printf("Il vous reste %d d'endurance.\n", endurance);
+        if (endurance < 100) {
+          printf("Voulez-vous vous reposer et recuperer 50 d'endurance. ? Non[0], Oui[1].\n", endurance);
+              scanf("%d", &choixRepos);
+                if (choixRepos == 1) {
+                  endurance = endurance + repos;
+                    if (endurance > 100) {
+                      printf("Vous ne pouvez pas avoir plus de 100 points d'endurance.\n");
+                      endurance = 100;
+                      printf("Vous avez %d\n", endurance);
+                    }
+                }
+        }
+        Color(15,0);
       } else if (strcmp(alqueir.nom, choixVille) == 0) {
         printf("Vous avez atteint la ville d'%s.\n", alqueir.nom);
         printf("%s\n", alqueir.description);
         strcpy(lieuActuel.nom, alqueir.nom);
         lieuActuel.ID = alqueir.ID;
+        lieuActuel.difficulte = alqueir.difficulte;
+        Color(5,0);
+        endurance = endurance - lieuActuel.difficulte;
+        if (endurance < 100) {
+          printf("Voulez-vous vous reposer et recuperer 50 d'endurance. ? Non[0], Oui[1].\n", endurance);
+          scanf("%d", &choixRepos);
+            if (choixRepos == 1) {
+              endurance = endurance + repos;
+                if (endurance > 100) {
+                  printf("Vous ne pouvez pas avoir plus de 100 points d'endurance.\n");
+                  endurance = 100;
+                  printf("Vous avez %d\n", endurance);
+                }
+            }
+        }
+        printf("Voulez-vous vous reposer et recuperer 50 d'endurance ?\n");
+        Color(15,0);
       } else if (strcmp(zoinrel.nom, choixVille) == 0) {
         printf("Vous avez atteint la ville de %s.\n", zoinrel.nom);
         printf("%s\n", zoinrel.description);
         strcpy(lieuActuel.nom, zoinrel.nom);
         lieuActuel.ID = zoinrel.ID;
+        lieuActuel.difficulte = zoinrel.difficulte;
+        Color(5,0);
+        endurance = endurance - lieuActuel.difficulte;
+        printf("Il vous reste %d d'endurance.\n", endurance);
+        Color(15,0);
       } else if (strcmp(ya.nom, choixVille) == 0) {
         printf("Vous avez atteint la ville de %s.\n", ya.nom);
         printf("%s\n", ya.description);
         strcpy(lieuActuel.nom, ya.nom);
         lieuActuel.ID = ya.ID;
+        lieuActuel.difficulte = ya.difficulte;
+        Color(5,0);
+        endurance = endurance - lieuActuel.difficulte;
+        printf("Il vous reste %d d'endurance.\n", endurance);
+        Color(15,0);
       } else if (strcmp(sirasp.nom, choixVille) == 0) {
         printf("Vous avez atteint la ville de %s.\n", sirasp.nom);
         printf("%s\n", sirasp.description);
         strcpy(lieuActuel.nom, sirasp.nom);
         lieuActuel.ID = sirasp.ID;
+        lieuActuel.difficulte = sirasp.difficulte;
+        Color(5,0);
+        endurance = endurance - lieuActuel.difficulte;
+        printf("Il vous reste %d d'endurance.\n", endurance);
+        Color(15,0);
       }
 
 
@@ -107,8 +165,21 @@ while (continuer == 1) {
 
 
           if (strcmp(lieuActuel.nom, belese.nom) == 0) { // aiguillage Belese
+            if (endurance < 100) {
+              printf("Voulez-vous vous reposer et recuperer 50 d'endurance. ? Non[0], Oui[1].\n", endurance);
+              scanf("%d", &choixRepos);
+                if (choixRepos == 1) {
+                  endurance = endurance + repos;
+                    if (endurance > 100) {
+                      printf("Vous ne pouvez pas avoir plus de 100 points d'endurance.\n");
+                      printf("Vous recuperer de l'endurance.\n");
+                      endurance = 100;
+                      printf("Vous avez %d\n", endurance);
+                    }
+                }
+            }
             printf("Dans quelle ville souhaitez-vous vous rendre ensuite ? Entrez l'ID de la ville choisie : ID %s: %d, ID %s: %d, ID %s: %d, ID %s: %d\n",alqueir.nom, alqueir.ID,zoinrel.nom, zoinrel.ID,ya.nom, ya.ID,sirasp.nom, sirasp.ID);
-            printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID);
+          Color(4,0);  printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID); Color(15,0);
             scanf("%d", &choixID);
               while ((choixID != lieuActuel.ID +1) && (choixID != lieuActuel.ID -1)) {
                 printf("Desole, vous ne pouvez pas acceder a cette ville.\n");
@@ -120,10 +191,26 @@ while (continuer == 1) {
                   printf("%s\n", alqueir.description);
                   strcpy(lieuActuel.nom, alqueir.nom);
                   lieuActuel.ID = alqueir.ID;
+                  lieuActuel.difficulte = alqueir.difficulte;
+                  endurance = endurance - lieuActuel.difficulte;
+                  printf("Il vous reste %d d'endurance.\n", endurance);
                 }
               } else if (strcmp(lieuActuel.nom, alqueir.nom) == 0) { // aiguillage Alqueir
+                if (endurance < 100) {
+                  printf("Voulez-vous vous reposer et recuperer 50 d'endurance. ? Non[0], Oui[1].\n", endurance);
+                  scanf("%d", &choixRepos);
+                    if (choixRepos == 1) {
+                      endurance = endurance + repos;
+                        if (endurance > 100) {
+                          printf("Vous ne pouvez pas avoir plus de 100 points d'endurance.\n");
+                          endurance = 100;
+                          printf("Vous recuperer de l'endurance.\n");
+                          printf("Vous avez %d\n", endurance);
+                        }
+                    }
+                }
                 printf("Dans quelle ville souhaitez-vous vous rendre ensuite ? Entrez l'ID de la ville choisie : ID %s: %d, ID %s: %d, ID %s: %d, ID %s: %d\n",belese.nom, belese.ID,zoinrel.nom, zoinrel.ID,ya.nom, ya.ID,sirasp.nom, sirasp.ID);
-                printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID);
+              Color(4,0);  printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID); Color(15,0);
                 scanf("%d", &choixID);
                   while ((choixID != lieuActuel.ID +1) && (choixID != lieuActuel.ID -1)) {
                     printf("Desole, vous ne pouvez pas acceder a cette ville.\n");
@@ -135,15 +222,21 @@ while (continuer == 1) {
                       printf("%s\n", belese.description);
                       strcpy(lieuActuel.nom, belese.nom);
                       lieuActuel.ID = belese.ID;
+                      lieuActuel.difficulte = belese.difficulte;
+                      endurance = endurance - lieuActuel.difficulte;
+                      printf("Il vous reste %d d'endurance.\n", endurance);
                     } else if (choixID == ya.ID) {
                       printf("Vous avez atteint la ville de %s.\n", ya.nom);
                       printf("%s\n", ya.description);
                       strcpy(lieuActuel.nom, ya.nom);
                       lieuActuel.ID = ya.ID;
+                      lieuActuel.difficulte = ya.difficulte;
+                      endurance = endurance - lieuActuel.difficulte;
+                      printf("Il vous reste %d d'endurance.\n", endurance);
                     }
-                }   else if (strcmp(lieuActuel.nom, ya.nom) == 0) { // aiguillage Alqueir
+                }   else if (strcmp(lieuActuel.nom, ya.nom) == 0) { // aiguillage Ya
                       printf("Dans quelle ville souhaitez-vous vous rendre ensuite ? Entrez l'ID de la ville choisie : ID %s: %d, ID %s: %d, ID %s: %d, ID %s: %d\n",belese.nom, belese.ID,zoinrel.nom, zoinrel.ID,alqueir.nom, alqueir.ID,sirasp.nom, sirasp.ID);
-                      printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID);
+                    Color(4,0);  printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID); Color(15,0);
                       scanf("%d", &choixID);
                         while ((choixID != lieuActuel.ID +1) && (choixID != lieuActuel.ID -1)) {
                           printf("Desole, vous ne pouvez pas acceder a cette ville.\n");
@@ -155,15 +248,21 @@ while (continuer == 1) {
                             printf("%s\n", alqueir.description);
                             strcpy(lieuActuel.nom, alqueir.nom);
                             lieuActuel.ID = alqueir.ID;
+                            lieuActuel.difficulte = alqueir.difficulte;
+                            endurance = endurance - lieuActuel.difficulte;
+                            printf("Il vous reste %d d'endurance.\n", endurance);
                           } else if (choixID == zoinrel.ID) {
                             printf("Vous avez atteint la ville de %s.\n", zoinrel.nom);
                             printf("%s\n", zoinrel.description);
                             strcpy(lieuActuel.nom, zoinrel.nom);
                             lieuActuel.ID = zoinrel.ID;
+                            lieuActuel.difficulte = zoinrel.difficulte;
+                            endurance = endurance - lieuActuel.difficulte;
+                            printf("Il vous reste %d d'endurance.\n", endurance);
                           }
-                      }    else if (strcmp(lieuActuel.nom, zoinrel.nom) == 0) { // aiguillage Alqueir
+                      }    else if (strcmp(lieuActuel.nom, zoinrel.nom) == 0) { // aiguillage Zoinrel
                             printf("Dans quelle ville souhaitez-vous vous rendre ensuite ? Entrez l'ID de la ville choisie : ID %s: %d, ID %s: %d, ID %s: %d, ID %s: %d\n",belese.nom, belese.ID,ya.nom, ya.ID,alqueir.nom, alqueir.ID,sirasp.nom, sirasp.ID);
-                            printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID);
+                          Color(4,0);  printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID); Color(15,0);
                             scanf("%d", &choixID);
                               while ((choixID != lieuActuel.ID +1) && (choixID != lieuActuel.ID -1)) {
                                 printf("Desole, vous ne pouvez pas acceder a cette ville.\n");
@@ -173,17 +272,50 @@ while (continuer == 1) {
                                  if (choixID == sirasp.ID) {
                                   printf("Vous avez atteint la ville d'%s.\n", sirasp.nom);
                                   printf("%s\n", sirasp.description);
+                                  if (avantPoste == 1) {
+                                    printf("%s\n", pancarte);
+                                  }
                                   strcpy(lieuActuel.nom, sirasp.nom);
                                   lieuActuel.ID = sirasp.ID;
+                                  lieuActuel.difficulte = sirasp.difficulte;
+                                  endurance = endurance - lieuActuel.difficulte;
+                                  printf("Il vous reste %d d'endurance.\n", endurance);
+                                      if (lieuActuel.difficulte > 10) { // AP
+                                        printf("Cette ville est dangereuse, souhaitez-vous y installer un avant-poste et reduire de moitie la difficulte ? Oui [1], Non[0].\n"); // AVANT POSTE.
+                                          scanf("%d", &avantPoste);
+                                            if (avantPoste == 1) {
+                                              printf("Vous installez un avant poste qui reduit la difficulte de %s.\n", lieuActuel.nom);
+                                              sirasp.difficulte = sirasp.difficulte/2;
+                                              printf("%s\n", pancarte);
+                                  //            strcat(sirasp.description, pancarte);
+                                            } else {
+                                              printf("Vous preferez ne pas installer d'avant-poste.\n");
+                                            }
+                            }
                                 } else if (choixID == ya.ID) {
                                   printf("Vous avez atteint la ville de %s.\n", ya.nom);
                                   printf("%s\n", ya.description);
                                   strcpy(lieuActuel.nom, ya.nom);
                                   lieuActuel.ID = ya.ID;
+                                  lieuActuel.difficulte = ya.difficulte;
+                                  endurance = endurance - lieuActuel.difficulte;
+                                  printf("Il vous reste %d d'endurance.\n", endurance);
                                 }
-                            } else if (strcmp(lieuActuel.nom, sirasp.nom) == 0) { // aiguillage Alqueir
+                            } else if (strcmp(lieuActuel.nom, sirasp.nom) == 0) { // aiguillage Sirasp
+                              if (lieuActuel.difficulte > 10) { // AP
+                                printf("Cette ville est dangereuse, souhaitez-vous y installer un avant-poste et reduire de moitie la difficulte ? Oui [1], Non[0].\n"); // AVANT POSTE.
+                                  scanf("%d", &avantPoste);
+                                    if (avantPoste == 1) {
+                                      printf("Vous installez un avant poste qui reduit la difficulte de %s.\n", lieuActuel.nom);
+                                      sirasp.difficulte = sirasp.difficulte/2;
+                              //        strcat(sirasp.description, pancarte);
+                                    } else {
+                                      printf("Vous preferez ne pas installer d'avant-poste.\n");
+                                    }
+                              }
+
                                   printf("Dans quelle ville souhaitez-vous vous rendre ensuite ? Entrez l'ID de la ville choisie : ID %s: %d, ID %s: %d, ID %s: %d, ID %s: %d\n",belese.nom, belese.ID,ya.nom, ya.ID,alqueir.nom, alqueir.ID,zoinrel.nom, zoinrel.ID);
-                                  printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID);
+                    Color(4,0);   printf("Vous ne pouvez pas choisir les ID qui ne precedent ou ne suivent directement l'ID de %s qui est %d.\n", lieuActuel.nom, lieuActuel.ID); Color(15,0);
                                   scanf("%d", &choixID);
                                     while ((choixID != lieuActuel.ID +1) && (choixID != lieuActuel.ID -1)) {
                                       printf("Desole, vous ne pouvez pas acceder a cette ville.\n");
@@ -195,10 +327,13 @@ while (continuer == 1) {
                                         printf("%s\n", zoinrel.description);
                                         strcpy(lieuActuel.nom, zoinrel.nom);
                                         lieuActuel.ID = zoinrel.ID;
+                                        lieuActuel.difficulte = zoinrel.difficulte;
+                                        endurance = endurance - lieuActuel.difficulte;
+                                        printf("Il vous reste %d d'endurance.\n", endurance);
                                       }
                                 }
-
+Color(15,0);
 }
-
+Color(15,0);
   return 0;
 }
